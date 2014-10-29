88e029f90091982f73665845d2f5ea9bc27712ea
wyrmxd@gmail.com <wyrmxd@gmail.com>
2014-10-29 08:52:31 +0000

    Checking output file

--- week_01/employee_payroll/file_writer.rb
+++ week_01/employee_payroll/file_writer.rb
@@ -1,12 +1,21 @@
 class File_writer
 
 	def initialize(file_name)
-		@fd = File.new(file_name, 'w')
+		@file_name = file_name
+		check_file
+	end
+
+	def check_file
+		# TODO: check rigthfully the file
+		if File.exists?(@file_name) && File.file?(@file_name) && File.readable?(@file_name) then
+			File.delete(@file_name)
+		end
 	end
 
 	def write(employee)
-		@fd.write(employee.to_s + "\n")
-		# TODO: what about closing files
+		File.open(@file_name, 'a') do | f |
+			f.write(employee.to_s + "\n")
+		end
 	end
 
 end
