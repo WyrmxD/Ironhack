2abd065d179351e9a36b6f63b37a33b344f954af
wyrmxd@gmail.com <wyrmxd@gmail.com>
2014-10-30 22:26:51 +0000

    Very ugly code but printing slides

--- week_01/console_keynote/console.rb
+++ week_01/console_keynote/console.rb
@@ -30,19 +30,37 @@ class Console
 	def next_slide
 		if(@@actual_slide < @slides.count-1) then
 			@@actual_slide += 1
-			show_slide(@@actual_slide)
 		end
+		show_slide(@@actual_slide)
 	end
 
 	def previous_slide()
 		if(@@actual_slide > 0) then
 			@@actual_slide -= 1
-			show_slide(@@actual_slide)
 		end
+		show_slide(@@actual_slide)
 	end
 
 	def show_slide(slide_number)
-		puts @slides[slide_number].content
+		height, width = TermInfo.screen_size
+		slide_text = @slides[slide_number].content
+
+		num_text_lines = slide_text.split("\n").count
+		num_blanks_before = (height.to_i - num_text_lines - 2 ) / 2
+		num_blanks_after = ( (height.to_i - num_text_lines - 2 ) / 2.0 ).ceil
+
+		for i in 1..(num_blanks_before) do
+			puts 
+		end
+		slide_text.split("\n").each do | text_line |
+			left_margin = (width - text_line.size) / 2
+			puts " " * left_margin + text_line.gsub("\n","\n" + " " * left_margin)
+		end
+		
+		for i in 1..(num_blanks_after) do
+			puts 
+		end
+		puts "[slide ##{@@actual_slide+1}]"
 	end
 
 end
